b816b7a311e7797b6f53f2e4030259f89eb88496
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-05-02 19:26:57 +0200

    Persistance layer is now complete and so is the database halleluja

--- src/server/domain/DomainInterfaceImpl.java
+++ src/server/domain/DomainInterfaceImpl.java
@@ -5,6 +5,7 @@
  */
 package server.domain;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import java.util.List;
 import server.persistance.PersistanceInterface;
 import server.persistance.PersistanceInterfaceImpl;
@@ -15,23 +16,48 @@ import server.persistance.PersistanceInterfaceImpl;
  */
 public class DomainInterfaceImpl implements DomainInterface {
 
-    private PersistanceInterface persistanceInterface = new PersistanceInterfaceImpl();
+    private final PersistanceInterface persistanceInterface = new PersistanceInterfaceImpl();
+    private String full_name = null;
+    private String userId = null;
+    private String ip = null;
+
+    public DomainInterfaceImpl(String ip) {
+        this.ip = ip;
+    }
 
     @Override
     public List<String[]> parseQuery(String[] query) {
-        switch (query[0]) {
-            case "login":
-                query[0] = "checkCredentials";
-                return persistanceInterface.parseQuery(query);
-            case "getMessages":
-                return persistanceInterface.parseQuery(query);
-            case "getUsers":
-                List<String[]> userData = persistanceInterface.parseQuery(new String[]{"login", query[1], query[2]});
-                if (!userData.isEmpty()) {
-                    return persistanceInterface.parseQuery(new String[]{query[0], userData.get(0)[3]});
+        try {
+            List<String[]> data = persistanceInterface.parseQuery(new String[]{"checkCredentials", query[1], query[2]});
+            if (!data.isEmpty()) {
+                switch (query[0]) {
+                    case "login":
+                        addActivity(query[0], "", ip, userId);
+                        return data;
+                    case "getCalendar":
+                        addActivity(query[0], query[3] + ";:;" + query[4] + ";:;" + query[5], ip, userId);
+                        return persistanceInterface.parseQuery(new String[]{query[3], query[4], query[5]});
+                    case "getEventParticipants":
+                        return persistanceInterface.parseQuery(new String[]{query[3]});
+                    case "addCalendarEvent":
+                        String eventId = persistanceInterface.parseQuery(new String[]{query[3]});
+                    case "getMessages":
+                        return persistanceInterface.parseQuery(query);
+                    case "getUsers":
+                        List<String[]> userData = persistanceInterface.parseQuery(new String[]{"login", query[1], query[2]});
+                        if (!userData.isEmpty()) {
+                            return persistanceInterface.parseQuery(new String[]{query[0], userData.get(0)[3]});
+                        }
+                        break;
                 }
+            }
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException | InvalidArgumentException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
+    private void addActivity(String type, String specifics, String ip, String userId) {
+        persistanceInterface.parseQuery(new String[]{type, specifics, ip, userId});
+    }
 }
