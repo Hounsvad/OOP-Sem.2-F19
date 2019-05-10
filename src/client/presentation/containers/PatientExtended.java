28454b0818973ebd4ba3aee231d2270a84ad6e2a
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-05-10 17:57:43 +0200

    updated patient extended with a compare method and a toString

--- src/client/presentation/containers/PatientExtended.java
+++ src/client/presentation/containers/PatientExtended.java
@@ -3,28 +3,49 @@
  */
 package client.presentation.containers;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Hounsvad
  */
-public class PatientExtended extends Patient {
+public class PatientExtended extends Patient implements Comparable<PatientExtended> {
+
+    private String department;
 
     /**
      *
      * @param fullName  is the full name of the patient
      * @param patientID
      */
-    public PatientExtended(String fullName, String patientID) {
+    public PatientExtended(String fullName, String patientID, String department) {
         super(fullName, patientID);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
     }
 
     /**
+     * Formats the result for showing in a listView
      *
      * @return a formatted string containing the patients name and id
      */
     @Override
     public String toString() {
-        return String.format("", super.toString());
+        return String.format("%s(%s)", getFullName(), getPatientID());
+    }
+
+    @Override
+    public int compareTo(PatientExtended o) {
+
+        Integer[] oTokens = (Integer[]) Arrays.asList(o.getDepartment().split("-")).stream().map(t -> Integer.parseInt(t)).collect(Collectors.toList()).toArray();
+        Integer[] tokens = (Integer[]) Arrays.asList(this.getDepartment().split("-")).stream().map(t -> Integer.parseInt(t)).collect(Collectors.toList()).toArray();
+        if (o.department.split("")) {
+
+        }
     }
 
 }
