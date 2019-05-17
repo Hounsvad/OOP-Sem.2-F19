0d922dcfbc9af7ee25a9b3dae3db6dd2a509a864
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-05-17 21:57:45 +0200

    Polished Presentation.java

--- src/client/presentation/Presentation.java
+++ src/client/presentation/Presentation.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Developed by SI2-PRO Group 3
  * Frederik Alexander Hounsvad, Oliver Lind Nordestgaard, Patrick Nielsen, Jacob Kirketerp Andersen, Nadin Fariss
  */
@@ -6,7 +6,6 @@ package client.presentation;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,14 +20,7 @@ import javafx.stage.WindowEvent;
  */
 public class Presentation extends Application {
 
-    /**
-     * The offset on the x axis
-     */
     private double xOffset = 0;
-
-    /**
-     * The offset on the y axis
-     */
     private double yOffset = 0;
 
     /**
@@ -39,8 +31,11 @@ public class Presentation extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
+        //Loads the login-screen
         FXMLLoader loader = new FXMLLoader(getClass().getResource("utils/credentials/LoginPopupFXML.fxml"));
         Parent root = loader.load();
+
+        //Makes the window draggable
         root.setOnMousePressed(event -> {
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
@@ -49,6 +44,8 @@ public class Presentation extends Application {
             stage.setX(event.getScreenX() - xOffset);
             stage.setY(event.getScreenY() - yOffset);
         });
+
+        //Removes the original decorator and loads the scene
         stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/client/presentation/css/generalStyleSheet.css").toExternalForm());
@@ -57,12 +54,10 @@ public class Presentation extends Application {
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/client/presentation/resources/sanitaslogo.png")));
         stage.show();
 
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent e) {
-                Platform.exit();
-                System.exit(0);
-            }
+        //Makes the entire program close on attempting to exit the the program
+        stage.setOnCloseRequest((WindowEvent e) -> {
+            Platform.exit();
+            System.exit(0);
         });
     }
 
