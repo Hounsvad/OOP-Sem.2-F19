a09028f7c03bbb2e178d8cafd221e1e92f35fdd5
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-05-17 21:36:22 +0200

    Polished SettingsPopupFXMLController

--- src/client/presentation/SettingsPopupFXMLController.java
+++ src/client/presentation/SettingsPopupFXMLController.java
@@ -1,22 +1,21 @@
-/* 
+/*
  * Developed by SI2-PRO Group 3
  * Frederik Alexander Hounsvad, Oliver Lind Nordestgaard, Patrick Nielsen, Jacob Kirketerp Andersen, Nadin Fariss
  */
 package client.presentation;
 
 import client.presentation.utils.credentials.CredentialContainer;
+import com.google.common.hash.Hashing;
 import com.jfoenix.controls.JFXPasswordField;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ResourceBundle;
-import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -38,8 +37,6 @@ public class SettingsPopupFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //Do nothing
-        //Note: https://stackoverflow.com/questions/48345922/reference-password-validation
-        //Propper password rules^^^
     }
 
     @FXML
@@ -49,69 +46,46 @@ public class SettingsPopupFXMLController implements Initializable {
 
     @FXML
     private void saveOwnPassword(ActionEvent event) {
+        //Saves the password to the database if valid
         if (validate()) {
-            CommunicationHandler.getInstance().sendQuery("alterOwnPassword", com.google.common.hash.Hashing.sha256().hashString(password.getText(), Charset.forName("UTF-8")).toString());
+            CommunicationHandler.getInstance().sendQuery("alterOwnPassword", Hashing.sha256().hashString(password.getText(), Charset.forName("UTF-8")).toString());
             CredentialContainer.getInstance().openLoginWindow();
             close();
-        } else {
-            if (validateEqual()) {
-                shakePassword();
-            } else {
-                shakeRepeat();
-            }
         }
     }
 
     @FXML
     private void updatePassword(KeyEvent event) {
+        //updates the css of the password field based on the password
         if (validatePassword()) {
-            System.out.println("password long");
             if (password.getStyleClass().contains("wrong-credentials")) {
                 password.getStyleClass().remove("wrong-credentials");
             }
-        } else {
-            shakePassword();
         }
     }
 
     @FXML
     private void updateRepeat(KeyEvent event) {
+        ////updates the css of the repeatPassword field based on the if the passwords are equal
         if (validateEqual()) {
-            System.out.println("passwords equal");
             if (repeatPassword.getStyleClass().contains("wrong-credentials")) {
                 repeatPassword.getStyleClass().remove("wrong-credentials");
             }
-        } else {
-            shakeRepeat();
         }
     }
 
     private boolean validate() {
+        //Checks if the password is valid and equal
         return validatePassword() && validateEqual();
     }
 
     private boolean validatePassword() {
+        //Checks if the password is valid
         return password.getText().length() >= 8;
     }
 
     private boolean validateEqual() {
+        //Checks if the password fields are equal
         return password.getText().equals(repeatPassword.getText());
     }
-
-    private void shakePassword() {
-        if (!password.getStyleClass().contains("wrong-credentials")) {
-            password.getStyleClass().add("wrong-credentials");
-        }
-        TranslateTransition translate = new TranslateTransition(Duration.millis(300), password);
-        translate.play();
-    }
-
-    private void shakeRepeat() {
-        if (!repeatPassword.getStyleClass().contains("wrong-credentials")) {
-            repeatPassword.getStyleClass().add("wrong-credentials");
-        }
-        TranslateTransition translate = new TranslateTransition(Duration.millis(300), repeatPassword);
-        translate.play();
-    }
-
 }
