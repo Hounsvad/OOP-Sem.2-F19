3d536aef987e9d0542c58320384db63b81b6c8f3
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-05-20 09:22:12 +0200

    Licence

--- src/client/presentation/containers/entries/Entry.java
+++ src/client/presentation/containers/entries/Entry.java
@@ -1,3 +1,7 @@
+/* 
+ * Developed by SI2-PRO Group 3
+ * Frederik Alexander Hounsvad, Oliver Lind Nordestgaard, Patrick Nielsen, Jacob Kirketerp Andersen, Nadin Fariss
+ */
 package client.presentation.containers.entries;
 
 /*This is code written by Frederik Alexander Hounsvad
